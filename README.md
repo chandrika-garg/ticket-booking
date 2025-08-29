3c3dd389e894529c21a113c70a1876c33cacc623
chandrika-garg <chandrika.abes@gmail.com>
2025-08-29 23:19:41 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,1 @@
+![Movie Booking DB Schema](./assests/img_1.png)
--- src/main/java/com/example/test/service/BookingService.java
+++ src/main/java/com/example/test/service/BookingService.java
@@ -1,6 +1,5 @@
 package com.example.test.service;
 
-import com.example.test.core.repository.BookingDetailsDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,9 +7,6 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class BookingService {
-
-    @Autowired
-    BookingDetailsDao bookingDetailsDao;
 
 
 
